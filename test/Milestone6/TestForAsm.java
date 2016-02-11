8944c260aae6d4a7fd7b2c3186e5abb7f3b14152
Ashok Raja <rajaa@rose-hulman.edu>
2016-02-11 11:01:10 -0500

    TestM6

--- test/Milestone6/TestForAsm.java
+++ test/Milestone6/TestForAsm.java
@@ -2,7 +2,6 @@ package Milestone6;
 
 import static org.junit.Assert.assertEquals;
 
-
 import java.io.FileOutputStream;
 
 import java.util.ArrayList;
@@ -81,20 +80,19 @@ public class TestForAsm {
 		detect.detect(m);
 		System.out.println(m);
 		for (IClass c : m.getClasses()) {
-		//	System.out.println(c.getName()+"boo");
-			System.out.println(c.getFields()+"baa");
-			
+			// System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields() + "baa");
+
 			if (c.getName().equals("problem/sprites/CompositeSprite")) {
-				//System.out.println(c.getName() + "blah");
+				// System.out.println(c.getName() + "blah");
 				assertEquals(true, c.getTags().contains("Composite"));
-			//	assertEquals(true,c.getFields().size()!=0);
+				// assertEquals(true,c.getFields().size()!=0);
 			}
 
 		}
 
 	}
-	
-	
+
 	@Test
 	public void TestComposite2() throws Exception {
 		List<String> cs = new ArrayList<>();
@@ -130,23 +128,22 @@ public class TestForAsm {
 		detect.detect(m);
 		System.out.println(m);
 		for (IClass c : m.getClasses()) {
-			//System.out.println(c.getName()+"boo");
-			System.out.println(c.getFields()+"baa");
-			
+			// System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields() + "baa");
+
 			if (c.getName().equals("java/lang/Iterable")) {
-				//System.out.println(c.getName() + "blah");
+				// System.out.println(c.getName() + "blah");
 				assertEquals(false, c.getTags().contains("composite"));
-				assertEquals(false,c.getFields().size()!=0);
+				assertEquals(false, c.getFields().size() != 0);
 			}
-		//	System.out.println(c.getFields());
-			
-			//if(c.getTags().equals(o))
+			// System.out.println(c.getFields());
+
+			// if(c.getTags().equals(o))
 
 		}
 
 	}
-	
-	
+
 	@Test
 	public void TestComposite3() throws Exception {
 		List<String> cs = new ArrayList<>();
@@ -182,25 +179,19 @@ public class TestForAsm {
 		detect.detect(m);
 		System.out.println(m);
 		for (IClass c : m.getClasses()) {
-			//System.out.println(c.getName()+"boo");
-			System.out.println(c.getFields()+"baa");
-			
+			// System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields() + "baa");
+
 			if (c.getName().equals("problem/sprites/CrystalBall")) {
-				//System.out.println(c.getName() + "blah");
+				// System.out.println(c.getName() + "blah");
 				assertEquals(false, c.getTags().contains("Composite"));
-				
-				
-				
-				
-				
+
 			}
-		
 
 		}
 
 	}
-	
-	
+
 	@Test
 	public void TestComposite4() throws Exception {
 		List<String> cs = new ArrayList<>();
@@ -236,27 +227,22 @@ public class TestForAsm {
 		detect.detect(m);
 		System.out.println(m);
 		for (IClass c : m.getClasses()) {
-			//System.out.println(c.getName()+"boo");
-			System.out.println(c.getFields()+"baa");
-			
+			// System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields() + "baa");
+
 			if (c.getName().equals("problem/sprites/ISprite")) {
-				//System.out.println(c.getName() + "blah");
+				// System.out.println(c.getName() + "blah");
 				assertEquals(false, c.getTags().contains("Composite"));
-				assertEquals(true,c.getFields().size()==0);
-				//assertEquals(true, );
-				
-				
-				
-				//assertEquals(false,c.getFields().size()>1);
+				assertEquals(true, c.getFields().size() == 0);
+				// assertEquals(true, );
+
+				// assertEquals(false,c.getFields().size()>1);
 			}
 
 		}
 
 	}
-	
-	
-	
-	
+
 	@Test
 	public void TestComposite5() throws Exception {
 		List<String> cs = new ArrayList<>();
@@ -292,20 +278,19 @@ public class TestForAsm {
 		detect.detect(m);
 		System.out.println(m);
 		for (IClass c : m.getClasses()) {
-			//System.out.println(c.getName()+"boo");
-			System.out.println(c.getFields()+"baa");
-			
+			// System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields() + "baa");
+
 			if (c.getName().equals("problem/sprites/NoLongerComposite")) {
-				//System.out.println(c.getName() + "blah");
+				// System.out.println(c.getName() + "blah");
 				assertEquals(false, c.getTags().contains("Composite"));
-				
+
 			}
 
 		}
 
 	}
-	
-	
+
 	@Test
 	public void TestComposite6() throws Exception {
 		List<String> cs = new ArrayList<>();
@@ -341,23 +326,69 @@ public class TestForAsm {
 		detect.detect(m);
 		System.out.println(m);
 		for (IClass c : m.getClasses()) {
-			//System.out.println(c.getName()+"boo");
-			System.out.println(c.getFields()+"baa");
-			
+			// System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields() + "baa");
+
 			if (c.getName().equals("problem/sprites/NoLongerComposite2")) {
-				//System.out.println(c.getName() + "blah");
+				// System.out.println(c.getName() + "blah");
 				assertEquals(false, c.getTags().contains("Composite"));
-				
+
 			}
 
 		}
 
 	}
-	
-	
-	
+
 	@Test
 	public void TestComposite7() throws Exception {
+		List<String> cs = new ArrayList<>();
+		cs.add("CompositeExample/Details");
+
+		IModel m = new Model();
+
+		IDetector detect = new CompositeDetector();
+		List<String> classRead = new ArrayList<>();
+
+		while (!cs.isEmpty()) {
+			String clazz = cs.get(0);
+			cs.remove(0);
+
+			ClassReader reader = new ClassReader(clazz);
+			IClass c = new Clazz();
+			// make class declaration visitor to get superclass and interfaces
+			ClassVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, c, m, cs);
+			// DECORATE declaration visitor with field visitor
+			ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, decVisitor, c, m);
+			// DECORATE field visitor with method visitor
+			ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, c, m);
+
+			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+
+			if (!c.getName().contains("$") && !classRead.contains(clazz)) {
+				m.addClass(c);
+				classRead.add(clazz);
+			}
+
+		}
+
+		detect.detect(m);
+		System.out.println(m);
+		for (IClass c : m.getClasses()) {
+			// System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields() + "blah");
+
+			if (c.getName().equals("CompositeExample/Details")) {
+				// System.out.println(c.getName() + "blah");
+				assertEquals(true, c.getTags().contains("Composite"));
+
+			}
+
+		}
+
+	}
+
+	@Test
+	public void TestComposite8() throws Exception {
 		List<String> cs = new ArrayList<>();
 		cs.add("CompositeExample/CompositeExample");
 
@@ -391,17 +422,17 @@ public class TestForAsm {
 		detect.detect(m);
 		System.out.println(m);
 		for (IClass c : m.getClasses()) {
-			//System.out.println(c.getName()+"boo");
-			System.out.println(c.getFields()+"blah");
-			
+			// System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields() + "blah");
+
 			if (c.getName().equals("CompositeExample/CompositeExample")) {
-				//System.out.println(c.getName() + "blah");
+				// System.out.println(c.getName() + "blah");
 				assertEquals(false, c.getTags().contains("Composite"));
-				
+
 			}
 
 		}
 
 	}
-	
+
 }
