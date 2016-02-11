49d8e1df5309217d41698ed3f1956002a8000db0
Ashok Raja <rajaa@rose-hulman.edu>
2016-02-11 00:56:20 -0500

    M6 Test

--- test/Milestone6/TestForAsm.java
+++ test/Milestone6/TestForAsm.java
@@ -49,7 +49,7 @@ public class TestForAsm {
 	@Test
 	public void TestComposite1() throws Exception {
 		List<String> cs = new ArrayList<>();
-		cs.add("problem/sprites/NoLongerComposite");
+		cs.add("problem/sprites/AbstractSprite");
 
 		IModel m = new Model();
 
@@ -84,14 +84,11 @@ public class TestForAsm {
 		//	System.out.println(c.getName()+"boo");
 			System.out.println(c.getFields()+"baa");
 			
-			if (c.getName().equals("problem/sprites/AbstarctSprite")) {
+			if (c.getName().equals("problem/sprites/AbstractSprite")) {
 				//System.out.println(c.getName() + "blah");
 				assertEquals(true, c.getTags().contains("composite"));
-				assertEquals(true,c.getFields().size()!=0);
+			//	assertEquals(true,c.getFields().size()!=0);
 			}
-		//	System.out.println(c.getFields());
-			
-			//if(c.getTags().equals(o))
 
 		}
 
@@ -191,7 +188,7 @@ public class TestForAsm {
 			if (c.getName().equals("problem/sprites/CrystalBall")) {
 				//System.out.println(c.getName() + "blah");
 				assertEquals(false, c.getTags().contains("composite"));
-				//assertEquals(true, );
+				
 				
 				
 				
@@ -350,6 +347,56 @@ public class TestForAsm {
 			if (c.getName().equals("problem/sprites/NoLongerComposite2")) {
 				//System.out.println(c.getName() + "blah");
 				assertEquals(false, c.getTags().contains("composite"));
+				
+			}
+
+		}
+
+	}
+	
+	
+	
+	@Test
+	public void TestComposite7() throws Exception {
+		List<String> cs = new ArrayList<>();
+		cs.add("CompositeExample/CompositeExample");
+
+		IModel m = new Model();
+
+		IDetector detect = new CompositeDetector();
+		List<String> classRead = new ArrayList<>();
+
+		while (!cs.isEmpty()) {
+			String clazz = cs.get(0);
+			cs.remove(0);
+
+			ClassReader reader = new ClassReader(clazz);
+			IClass c = new Clazz();
+			// make class declaration visitor to get superclass and interfaces
+			ClassVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, c, m, cs);
+			// DECORATE declaration visitor with field visitor
+			ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, decVisitor, c, m);
+			// DECORATE field visitor with method visitor
+			ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, c, m);
+
+			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+
+			if (!c.getName().contains("$") && !classRead.contains(clazz)) {
+				m.addClass(c);
+				classRead.add(clazz);
+			}
+
+		}
+
+		detect.detect(m);
+		System.out.println(m);
+		for (IClass c : m.getClasses()) {
+			//System.out.println(c.getName()+"boo");
+			System.out.println(c.getFields()+"blah");
+			
+			if (c.getName().equals("CompositeExample/CompositeExample")) {
+				//System.out.println(c.getName() + "blah");
+				assertEquals(true, c.getTags().contains("composite"));
 				
 			}
 
