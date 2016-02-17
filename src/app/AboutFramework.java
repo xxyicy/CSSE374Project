dfa289be106fa43dc20fd2b3b3ae4ef888ead3c4
Ashok Raja <rajaa@rose-hulman.edu>
2016-02-16 21:41:18 -0500

    Added details

--- src/app/AboutFramework.java
+++ src/app/AboutFramework.java
@@ -20,8 +20,18 @@ public class AboutFramework extends JFrame {
 		this.setResizable(false);
 		this.setLayout(new GridLayout(6, 1));
 		JLabel message = new JLabel("About.....");
+		JLabel line1 = new JLabel("Our project has 3 parts");
+		JLabel line2 = new JLabel("1) Modified ASM part to read all classes and files");
+		JLabel line3 = new JLabel("2) Internal Representation of classes and traversers of visitor pattern");
+		JLabel line4 = new JLabel("3) Visitor of the visitor pattern which has string buffer to parse all class information ");
+		
 
 		this.add(message);
+		this.add(line1);
+		this.add(line2);
+		this.add(line3);
+		this.add(line4);
+		
 		this.pack();
 		this.setVisible(true);
 
--- src/app/HelpFramework.java
+++ src/app/HelpFramework.java
@@ -19,9 +19,18 @@ public class HelpFramework extends JFrame {
 		this.setPreferredSize(INITAL_SIZE);
 		this.setResizable(false);
 		this.setLayout(new GridLayout(6, 1));
-		JLabel message = new JLabel("How to go about the app..........");
+		JLabel message = new JLabel("How to go about the app...");
+		JLabel line1  = new JLabel("i) Open your Terminal window");
+		JLabel line2  = new JLabel("ii) Go to your directory");
+		JLabel line3  = new JLabel("iii) Parse the given directory to print the texture representation");
+		JLabel line4 = new JLabel("iv)To get UML:  dot -Tpng output/output.txt > <output-file-name>");
 		
 		this.add(message);
+		this. add(line1);
+		this. add(line2);
+		this. add(line3);
+		this. add(line4);
+		
 		this.pack();
 		this.setVisible(true);
 		
